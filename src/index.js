01fbb442b29ed9ab714a8e10c2d5b764330f243d
Alexandr <nasanovicha@gmail.com>
2022-02-23 09:17:32 +0300

    feat: completed morse decoder task

--- src/index.js
+++ src/index.js
@@ -39,8 +39,32 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let size = 10
+    let l = expr.length/size
+    let arr = []
+    
+    for (i=0; i<l; i++) {arr.push(expr.slice(i*size, size+i*size))}
+    // console.log('arr', arr)
+
+    let resultArr = []
+    for (i=0; i<arr.length; i++) {
+        if(arr[i] === '**********') {resultArr = resultArr.concat(' ')}
+    else{
+        let smallArr = []
+    for (j=0; j<5; j++) {
+        let el = arr[i].slice(j*2, j*2+2)
+        if (el==='10') {smallArr.push('.')}
+        if (el==='11') {smallArr.push('-')}
+    }
+    // console.log('smallArr', MORSE_TABLE[smallArr.join('')])
+    resultArr = resultArr.concat(MORSE_TABLE[smallArr.join('')])
+    }
+    }  
+    return resultArr.join('')
 }
 
 module.exports = {
     decode
 }
+
+// console.log(decode('00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010'))
